e729b4d3c83ec5960421ca0e8f8c93f462d99ff8
hp-no <91341531+hp-no@users.noreply.github.com>
2021-11-24 00:29:08 -0600

    changed material to make rotation more clear

--- sketch.js
+++ sketch.js
@@ -12,8 +12,8 @@ function setup() {
 
 function draw() {
   background(220);
-  rotateX(frameCount * 0.01);
+  
   rotateY(frameCount * 0.01);
-  normalMaterial(); //placehodler material
+  fill("pink");
   sphere();
 }
