e9ddb45057aecb71b4a68d94120a941c52cc4817
hp-no <91341531+hp-no@users.noreply.github.com>
2021-11-24 12:10:50 -0600

    starting development of rotation

--- sketch.js
+++ sketch.js
@@ -5,6 +5,9 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//Use setCamera() function for an opneing menu screen? or a finished product showcase?
+//Nice to have: a room of containing all created pieces to walk and look around in
+let speed = 10;
 
 function setup() {
   createCanvas(windowWidth*0.85, windowHeight*0.95, WEBGL);
@@ -12,8 +15,26 @@ function setup() {
 
 function draw() {
   background(220);
-  
-  rotateY(frameCount * 0.01);
-  fill("pink");
-  sphere(175, 16, 4);
+  if (keyIsDown(65)) {
+    rotateY(speed * 0.01);
+  }
+  // rotateY(frameCount * 0.01);
+  fill("orange");
+  sphere(width*0.1, 16, 4);
+  // rotationMovement();
+}
+
+function rotationMovement() {
+  if (keyIsDown(65)) {
+    speed += 5;
+  }
+  if (keyIsPressed(LEFT_ARROW)) {
+    rotateY(1000 * 0.01);
+  }
+  if (keyIsDown(68)) {
+    rotateY(-3 * 0.01);
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    rotateY(-3 * 0.01);
+  }
 }
