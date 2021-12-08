380457381e42d2826cfef7851c801111e26c0fda
hp-no <91341531+hp-no@users.noreply.github.com>
2021-12-08 11:47:06 -0600

    implementation of a 3D array

--- sketch.js
+++ sketch.js
@@ -11,10 +11,16 @@
 //"object" is a temporary variable name for the art piece
 
 
+
+
 //global variables
 let speed = 10;
 let objectdx = 0;
 let objectdy = 0;
+
+//grid parameters
+let gridSize = 20;
+let grid;
 
 //camera parameters
 let cameraZ = 0; //-100
@@ -25,34 +31,64 @@ let centerZ = 100; //75
 function setup() {
   createCanvas(windowWidth*0.85, windowHeight*0.95, WEBGL);
   angleMode(DEGREES);
+
+  grid = createEmpty3DArray(gridSize, gridSize, gridSize);
 }
 
 function draw() {
   background(220);
-  displayObject();
-  rotationMovement();
+  // displayObject();
+  // rotationMovement();
   displayCamera();
-  perspectiveMovement();
-  displayBoxes();
+  // perspectiveMovement();
+  display3DGrid();
+
 }
 
 function displayCamera() {
   translate(0, 0, 0);
-  camera(-150 , -300, cameraZ, //-150, -300, camZ
-    centerX, centerY, centerZ,
-    0, 1, 0);
+  // camera(-150 , -300, cameraZ, //-150, -300, camZ
+  //   centerX, centerY, centerZ,
+  //   0, 1, 0);
   // camera(0, 0, cameraZ, centerX, centerY, centerZ, 0, 1, 0); // explore the functions of the last 3 parameters
+
+  camera(-100, -100, -50,
+    0, 0, 50,
+    0, 1, 0);
 }
 
-function displayBoxes() {
-  for (let x=0; x<width; x+=20) {
-    for (let z=0; z<height; z+=20) {
-      push();
-      translate(x-10, 0, z-10); //sets the xyz location for each box
-      box(20);
-      pop();
+function display3DGrid() {
+  for (let z=0; z<gridSize; z++) {
+    for (let y=0; y<gridSize; y++) {
+      for (let x=0; x<gridSize; x++) {
+        if (grid[z][y][x] === 0) {
+          fill("white");
+        }
+        else if (grid[z][y][x] === 1) {
+          fill("black");
+        }
+        push();
+        translate(x*20, y*20, z*20); //sets the xyz location for each box
+        box(20);
+        pop();
+      }
     }
   }
+}
+
+
+function createEmpty3DArray(rows, cols, layers) {
+  let grid = [];
+  for (let z=0; z<layers; z++) {
+    grid.push([]);
+    for (let y=0; y<rows; y++) {
+      grid[z].push([]);
+      for (let x=0; x<cols; x++) {
+        grid[z][y].push(0);
+      }
+    }
+  }
+  return grid;
 }
 
 function displayObject() {
