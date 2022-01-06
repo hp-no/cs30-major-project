3ec354bc563dcda296fbfb768d97ffc80998d885
hp-no <91341531+hp-no@users.noreply.github.com>
2022-01-06 12:11:05 -0600

    restructured code for clarity

---
+++ testing-code/sketch.js
@@ -0,0 +1,126 @@
+// Base Sample
+
+//global variables
+let grid;
+let gridSize = 20;
+let cellSize = 15;
+
+// alt. variables:
+let d = 1; // represents distance/spacing between blocks
+let blockArray = [];
+
+
+function setup() {
+  createCanvas(windowWidth*0.85, windowHeight*0.95, WEBGL);
+  angleMode(DEGREES);
+  grid = create3DArray(gridSize, gridSize, gridSize);
+}
+
+function draw() {
+  background(220);
+  displayCamera();
+  display3DGrid();
+}
+
+function displayCamera() {
+  translate(width/2, height/2, 0);
+  camera(-200, -200, -150, //dist.
+    0, 0, 50,
+    0, 1, 0);
+}
+
+function display3DGrid() {
+  for (let z=0; z<gridSize; z++) {
+    for (let y=0; y<gridSize; y++) {
+      for (let x=0; x<gridSize; x++) {
+        if (grid[z][y][x] !== 2) { // if NOT an empty space (2 = empty)
+          if (grid[z][y][x] === 0) {
+            fill("white");
+          }
+          else if (grid[z][y][x] === 1) {
+            fill("black"); 
+          }
+          push();
+          translate(x, y, z); //sets the xyz location for each box
+          stroke(200);
+          box(cellSize);
+          pop();
+        }
+      }
+    }
+  }
+}
+
+function create3DArray(rows, cols, layers) {
+  //currently a "randomized" 3D array
+  let grid = [];
+  for (let z=0; z<layers; z++) {
+    grid.push([]);
+    for (let y=0; y<rows; y++) {
+      grid[z].push([]);
+      for (let x=0; x<cols; x++) {
+        //create random 3D array
+        if (random(100) < 50) {
+          grid[z][y].push(2);
+        }
+        else if (random(100) > 50) {
+          grid[z][y].push(1);
+        }
+        else {
+          grid[z][y].push(0);
+        }
+        
+      }
+    }
+  }
+  return grid;
+}
+
+
+
+// Alternative Grid Method: //
+
+class Block {
+  constructor(x, y, z, cellSize, theColor) {
+    this.x = x*cellSize;
+    this.y = y*cellSize;
+    this.z = z*cellSize;
+    this.size = cellSize;
+    this.theColor = theColor; //placeholder used for testing
+  }
+
+  display() {
+    push();
+    translate(this.x*d, this.y*d, this.z*d); //sets the xyz location for each box
+    stroke(200);
+    fill(this.theColor);
+    box(this.size);
+    pop();
+  }
+}
+
+function altDisplay3DGrid() { // ALTERNATIVE way to display the grid
+  for (let z=0; z<gridSize; z++) {
+    for (let y=0; y<gridSize; y++) {
+      for (let x=0; x<gridSize; x++) {
+        if (grid[z][y][x] === 0) {
+          // fill("white");
+          let theBlock = new Block(x,y,z,cellSize,"white");
+          theBlock.display();
+          blockArray.push(theBlock);
+        }
+        else if (grid[z][y][x] === 1) {
+          // fill("black"); 
+          let theBlock = new Block(x,y,z,cellSize,"black");
+          theBlock.display();
+          blockArray.push(theBlock);
+        }
+        else if (grid[z][y][x] === 2) {
+          //empty space, doesn't use the block display function
+          let theBlock = new Block(x,y,z,cellSize,"white");
+          blockArray.push(theBlock);
+        }
+      }
+    }
+  }
+}
