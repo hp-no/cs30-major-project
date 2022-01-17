6b5b718e7b12c5ddf59281779c21c4469d4cd128
hp-no <91341531+hp-no@users.noreply.github.com>
2022-01-17 00:52:08 -0600

    backup of basic color switching mechanic

--- testing-code/sketch_test.js
+++ testing-code/sketch_test.js
@@ -1,31 +1,72 @@
-// Testing three.js mechanics
+// Backup of a base colour ver. (for reference)
 
+//global variables
+let rectColor = "white";
+
+let colorArray = [0xff0000, 0xffbf00, 0x00ff00, 0x0080ff, 0xbf00ff, 0xff66ff, 0xff0080];
+let cubeArray;
+let currentCube = 0; // represents position within the cube array
+
+
+// three.js setup
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 ); // incorporate this command in actual code
-camera.lookAt( 0, 0, 0 ); // this too
-
-const scene = new THREE.Scene();
-
-//create a blue LineBasicMaterial
-const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
-
-//create a geometry
-const points = [];
-points.push( new THREE.Vector3( - 10, 0, 0 ) );
-points.push( new THREE.Vector3( 0, 10, 0 ) );
-points.push( new THREE.Vector3( 10, 0, 0 ) );
-
-const geometry = new THREE.BufferGeometry().setFromPoints( points );
-
+camera.position.z = 5;
 
 
 
 function setup() {
+  createCanvas(windowWidth*0.85, windowHeight*0.95);
+
+  button = createButton("Color change");
+  button.position(width*0.20, height*0.9);
+  button.mousePressed(changeCubeColor);
+
+  cubeArray = createCubeArray();
+  scene.add(cubeArray[0]);
+}
+function draw() {
+  //p5js
+  fill(rectColor)
+  rect(width*0.15, height*0.86, 145, 75);
+
+  //three.js
+  rotateCube();
+  renderer.render( scene, camera );
 }
 
-function draw() {
+function rotateCube() {
+  cubeArray[currentCube].rotation.x += 0.01;
+  cubeArray[currentCube].rotation.y += 0.01;
 }
+
+
+function createCubeArray() {
+  let theArray = [];
+  for (let i=0; i<colorArray.length; i++) {
+    //create different coloured cubes
+    const geometry = new THREE.BoxGeometry();
+    let material = new THREE.MeshBasicMaterial( { color: colorArray[i] } );
+    const cube = new THREE.Mesh( geometry, material );
+
+    theArray.push(cube);
+  }
+  return theArray;
+}
+
+function changeCubeColor() {
+  //switch between different coloured cubes
+  scene.remove ( cubeArray[currentCube] ); // remove the current cube color
+  if (currentCube === cubeArray.length-1) {
+    currentCube = 0;
+  }
+  else {
+    currentCube++;
+  }
+  scene.add ( cubeArray[currentCube] ) ; // add the next cube color
+}
+
