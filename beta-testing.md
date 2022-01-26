3ef6326c3c6fd089428c338106106cd664559ea4
hp-no <91341531+hp-no@users.noreply.github.com>
2022-01-25 22:43:42 -0600

    incorporated more suggestions + added final comments

--- beta-testing.md
+++ beta-testing.md
@@ -1,5 +1,7 @@
-## Beta testing
+# Beta testing
 Constructive feedback + suggestions gathered from beta-testers.
+
+# Feedback
 
 ## Gerlie
 - Add an option to change the background colour
@@ -13,3 +15,10 @@ Constructive feedback + suggestions gathered from beta-testers.
 
 ## Paulie
 - Potentially add an undo function
+
+# What got added/changed:
+- Implemented another colour wheel to change BG colour
+- Added a "Preview" mode for the cube when the enter key is pressed
+- Fixed the painting function to not paint through the blocks behind the intended block
+- Fixed the sculpting tool to not sculpt through multiple layers simultaneously
+- Removed circle indicators underneath tool icons
--- sketch.js
+++ sketch.js
@@ -1,27 +1,26 @@
-// CS30 Major Project: (Draw/Art 3D)
+// CS30 Major Project - Cube Draw
 // Hannah Dechavez
 // January 25, 2022
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+
 
 // Variables
-let cubeGrid, paintCanvas, mouse, raycaster, controls, inputColor, bgColor;
+let cubeGrid, paintCanvas, mouse, raycaster, controls, inputColor, bgColor, removedObjects;
 let cubeImg, canvasImg, brushIcon, chiselIcon, bgm;
-let cubeButton, canvasButton, instructions, text1, text2, text3, text4, text5, text6, text7;
+let cubeButton, canvasButton, instructions, text1, text2, text3, text4, text5, text6, text7, text8;
 let paintColorText, bgColorText;
 
 let gridSize = 15;
-let spacing = 1;
 let theColor = 0xed225d;
-let someColor = 0xd3d3d3;
+let currentBgValue = 0xd3d3d3;
 
 let cDown = false;
 let cubeLoaded = false;
 let canvasLoaded = false;
 let showControls = false;
+let togglePreview = false;
 
 
-// three.js setup initialization
+// three.js setup
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
@@ -30,16 +29,18 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement) ;
 scene.background = new THREE.Color("lightgrey");
 
-//variables for mouse interaction
+//assigning variables for mouse interaction
 mouse = new THREE.Vector2();
 raycaster = new THREE.Raycaster();
 controls = new THREE.OrbitControls(camera, renderer.domElement); // mouse-drag camera movement
 
-// base scene setup
+// variable for storing deleted blocks
+removedObjects = new THREE.Object3D();
+
+// basic scene setup
 camera.position.set(7, 6.5, 8);
 camera.lookAt(0,0,0);
-scene.background = new THREE.Color(someColor);
-
+camera.layers.enableAll();
 const directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
 scene.add(directionalLight);
 
@@ -49,9 +50,11 @@ let centerCube = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({colo
 scene.add(centerCube);
 
 
+
 function loadCubeSetup () {
   // scene setup for the cube
   camera.position.set(17, 17, 17);
+  camera.lookAt(0,0,0);
   directionalLight.position.set(5, 5, 0);
 
   // create a 3D grid of cubes
@@ -60,15 +63,18 @@ function loadCubeSetup () {
     for (let y=0; y<gridSize; y++) {
       for (let x=0; x<gridSize; x++) {
         let box = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshToonMaterial({color: 0xffffff})); // white base colour
-        box.position.set((x-gridSize/2) * spacing, (y-gridSize/2) * spacing, (z-gridSize/2) * spacing);
+        box.position.set((x-gridSize/2), (y-gridSize/2), (z-gridSize/2));
 
         cubeGrid.add(box);
       }
     }
   }
+
+  // displays the cube grid by adding it to the scene
   scene.add(cubeGrid);
-  scene.add(centerCube);
   cubeLoaded = true;
+  togglePreview = false;
+
 }
 
 function loadPaintCanvas() {
@@ -76,33 +82,38 @@ function loadPaintCanvas() {
   camera.position.set(0, 4, 40);
   camera.lookAt(20, 20, 0);
   directionalLight.position.set(0, 0, 5);
-
+  
   // create a 2D grid of cubes
   paintCanvas = new THREE.Object3D();
   for (let y=-21; y<34; y++) { //-1 to 14
     for (let x= -25; x<37; x++) { //-5 to 17
       let box = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshToonMaterial({color: 0xffffff})); //blank sheet
-      box.position.set((x-gridSize/2) * spacing, (y-gridSize/2) * spacing, 0 * spacing);
+      box.position.set((x-gridSize/2), (y-gridSize/2), 0);
 
       paintCanvas.add(box);
     }
   }
+
+  // displays the flat canvas by adding it to the scene
   scene.add(paintCanvas);
-  scene.remove(centerCube);
   canvasLoaded = true;
+
 }
 
 function resetShape() {
-  if (cubeLoaded) { // reset cube
+  // resets user changes made to the cube/canvas
+  if (cubeLoaded) { // reset cube appearance
     for (let i=0; i<cubeGrid.children.length; i++) {
       if (cubeGrid.children[i].material) {
-        cubeGrid.children[i].material.opacity = 1;
+        cubeGrid.add(removedObjects);
         cubeGrid.children[i].material.color.set(0xffffff);
       }
     }
+    // empty the removedObjects variable
+    removedObjects = new THREE.Object3D; 
   }
 
-  if (canvasLoaded) { // reset/clear paint canvas
+  if (canvasLoaded) { // clear paint canvas
     for (let i=0; i<paintCanvas.children.length; i++) {
       if (paintCanvas.children[i].material) {
         paintCanvas.children[i].material.opacity = 1;
@@ -112,25 +123,31 @@ function resetShape() {
   }
 }
 
-function sculptBlock() { // when 'z' key is down
+function sculptBlock() {
+  // shapes the cube by deleting the intersected blocks
+  raycaster.setFromCamera(mouse, camera);
+
   if (cubeLoaded) {
-    raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects(cubeGrid.children);
     for (let i=0; i<intersects.length; i++) {
       intersects[i].object.material.transparent = true;
-      intersects[i].object.material.opacity = 0;
+
+      //store the removed objects in a variable
+      removedObjects.add(intersects[0].object);
+      cubeGrid.remove(intersects[0].object);
     }
   }
 }
 
-function paintBlock() { // when the 'c' key is down
+function paintBlock() {
+  // paints the cube/canvas by changing the color of the intersected blocks
   raycaster.setFromCamera(mouse, camera);
 
   if (cubeLoaded) { //painting on the cube
     const intersectsCube = raycaster.intersectObjects(cubeGrid.children);
     for (let i=0; i<intersectsCube.length; i++) { 
       if (intersectsCube[i].object.material.opacity !== 0) {
-        intersectsCube[i].object.material.color.set(theColor); // 0xdfff00
+        intersectsCube[0].object.material.color.set(theColor); // 0xdfff00
       }
     }
   }
@@ -156,22 +173,8 @@ function onMouseMove(event) {
   mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
 }
 
-// function onClick(event) { 
-//   raycaster.setFromCamera(mouse, camera);
-//   let intersects = raycaster.intersectObjects(sphereGrid.children);
-//   if (intersects.length > 0) {
-//     scene.remove(sphereGrid.children[0]);
-//   }
-// }
-
 animate();
 window.addEventListener("mousemove", onMouseMove, false);
-// window.addEventListener("click", onClick);
-
-
-
-
-
 
 function preload() {
   soundFormats("mp3");
@@ -186,15 +189,16 @@ function preload() {
 function setup() {
   createCanvas(windowWidth/13, windowHeight);
 
+  // create buttons to load the cube and paint canvas
   cubeButton = new Button(width*0.125, height*0.40, width*0.75, height*0.12, cubeImg);
   canvasButton = new Button(width*0.125, height*0.55, width*0.75, height*0.12, canvasImg);
 
-  // create a button to toggle captions for control keys
+  // create a button to toggle guide/instructions for control keys
   instructions = createButton("Controls");
   instructions.position(width*0.2, windowHeight*0.7);
   instructions.mousePressed(displayControls);
 
-  // add colour selection wheels for paintbrush and background
+  // add color selection wheels for the paintbrush and background
   inputColor = createColorPicker("#ed225d");
   inputColor.position(width*0.175, height*0.23);
   inputColor.size(75, 75);
@@ -206,9 +210,12 @@ function setup() {
 
 function draw() {
   background("indigo");
+
   displaySidebar();
   displayText();
+
   enableTools();
+  previewMode();
 }
 
 function mouseClicked() {
@@ -217,7 +224,7 @@ function mouseClicked() {
     bgm.loop();
   }
 
-  // if side buttons are pressed:
+  // loads the cube when button is pressed
   if (cubeButton.isPressed() && !cubeLoaded) {
     if (canvasLoaded) {
       scene.remove(paintCanvas);
@@ -225,6 +232,8 @@ function mouseClicked() {
     }
     loadCubeSetup();
   }
+
+  // loads the canvas when button is pressed
   if (canvasButton.isPressed() && !canvasLoaded) {
     if (cubeLoaded) {
       scene.remove(cubeGrid);
@@ -235,6 +244,7 @@ function mouseClicked() {
 }
 
 function displayText() {
+  // displays the text for the color wheels on the side bar
   paintColorText = createP("Brush colour");
   paintColorText.style("color", "lightgrey");
   paintColorText.style("font-size", "10pt");
@@ -244,8 +254,6 @@ function displayText() {
   bgColorText.style("color", "lightgrey");
   bgColorText.style("font-size", "10pt");
   bgColorText.position(width*0.25, height*0.13);
-
-
 }
 
 function displayControls() {
@@ -259,6 +267,7 @@ function displayControls() {
     text5 = createP("Mouse LEFT-CLICK and DRAG to rotate camera");
     text6 = createP("Mouse RIGHT-CLICk and DRAG to move camera position");
     text7 = createP("Use mouse scroll-wheel to zoom-in/out");
+    text8 = createP("Press 'ENTER' to toggle ON/OFF preview mode  **Only for the cube")
    
     text1.style("font-size", "10pt");
     text2.style("font-size", "10pt");
@@ -267,6 +276,7 @@ function displayControls() {
     text5.style("font-size", "10pt");
     text6.style("font-size", "10pt");
     text7.style("font-size", "10pt");
+    text8.style("font-size", "10pt");
   
     text1.position(140, height-110);
     text2.position(140, height-90);
@@ -275,8 +285,9 @@ function displayControls() {
     text5.position(550, height-110);
     text6.position(550, height-90);
     text7.position(550, height-70);
+    text8.position(550, height-50);
   }
-  else { // hide text
+  else { // hides text
     text1.remove();
     text2.remove();
     text3.remove();
@@ -284,6 +295,7 @@ function displayControls() {
     text5.remove();
     text6.remove();
     text7.remove();
+    text8.remove();
   }
 
 }
@@ -295,28 +307,22 @@ function displaySidebar() {
   rect(0, height*0.855, 120, 60);
   image(chiselIcon, width*0.09, height*0.86, 50, 50);
   image(brushIcon, width*0.5, height*0.863, 50, 50);
-  fill("lightgrey");
-  circle(width*0.25, height*0.965, 25); // indicator if sculpting is being done
-  circle(width*0.75, height*0.965, 25); // indicator if paint function is being used
 }
 
 function enableTools() {
-  // sculpt function
+  // enable sculpt function when held down
   if (keyIsDown(90)) {  // 'z' key
     sculptBlock();
-
     if (cubeLoaded) { //indicate that sculpting function is ON
       fill("lime");
       circle(width*0.25, height*0.965, 25);
     }
   }
 
-
-  // paint function
+  // enable paint function when held down
   if (keyIsDown(67)) { // 'c' key
     cDown = true;
     paintBlock();
-
     if (cubeLoaded || canvasLoaded) { //indicate that paint function is ON
       fill("lime");
       circle(width*0.75, height*0.965, 25); 
@@ -330,14 +336,41 @@ function enableTools() {
   if (inputColor.value() !== theColor) {
     theColor = new THREE.Color(inputColor.value());
   }
-  if (bgColor.value() !== someColor) {
-    someColor = new THREE.Color(bgColor.value());
+  if (bgColor.value() !== currentBgValue) {
+    currentBgValue = bgColor.value();
+    scene.background = new THREE.Color(bgColor.value());
+  }
+}
+
+function previewMode() { // for cube
+  if (togglePreview && cubeLoaded) {
+    cubeGrid.rotation.x += 0.01;
+    cubeGrid.rotation.y += 0.01;
+
+  }
+  else if (!togglePreview && cubeLoaded) {
+    cubeGrid.rotation.x = 0;
+    cubeGrid.rotation.y = 0;
   }
 }
 
 function keyPressed() {
-  if (keyCode === 32) { // SPACEBAR key
+  if (keyCode === 32) { // SPACEBAR key to reset shape/changes
     resetShape();
+  }
+
+  if (keyCode === 13) { // ENTER key to toggle preview mode for cube
+    togglePreview = !togglePreview;
+
+    if (togglePreview) { //hide controls
+      showControls = true;
+      displayControls();
+    }
+    if (cubeLoaded) { //reset camera position for cube
+      camera.position.set(17, 17, 17);
+      camera.lookAt(0, 0, 0);
+    }
+    
   }
 }
 
@@ -364,10 +397,10 @@ class Button {
   }
 }
 
-//put this in a credits section later...
+
 
 //BGM Music Used:
 
 /* "Late Night Radio" Kevin MacLeod (incompetech.com)
 Licensed under Creative Commons: By Attribution 4.0 License
-http://creativecommons.org/licenses/by/4.0/ */
+http://creativecommons.org/licenses/by/4.0/ */ 
